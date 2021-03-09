cf139a0809a75d49fa015a2c06aa0a1c09f0bb34
Ivan <polanisivan@gmail.com>
2021-03-09 18:15:35 -0300

    Desafío 12

--- js/data.js
+++ js/data.js
@@ -1,56 +1,55 @@
-const data = [
-    {   id: 1,
-        name: "I9 10900F",
-        type: "cpu",
-        price: 375,
-        description: "Lorem ipsum...",
-        stock: 100,
-        img: "assets/img/i9-10900f.jpg",
-        qty: 0
+const data =  [{  
+        "id": 1,
+        "name": "I9 10900F",
+        "type": "cpu",
+        "price": 375,
+        "description": "Lorem ipsum...",
+        "stock": 100,
+        "img": "assets/img/i9-10900f.jpg",
+        "qty": 0
     },{
-        id: 2,
-        name: "Ryzen 5 3600X",
-        type: "cpu",
-        price: 385,
-        description: "Lorem ipsum...",
-        stock: 100,
-        img: "assets/img/ryzen5-3600x.jpg",
-        qty: 0
+        "id": 2,
+        "name": "Ryzen 5 3600X",
+        "type": "cpu",
+        "price": 385,
+        "description": "Lorem ipsum...",
+        "stock": 100,
+        "img": "assets/img/ryzen5-3600x.jpg",
+        "qty": 0
     },{
-        id: 3,
-        name: "Zotac RTX 3090",
-        type: "gpu",
-        price: 2500,
-        description: "Lorem ipsum...",
-        stock: 100,
-        img: "assets/img/zotac_rtx_3090_1.jpg",
-        qty: 0
+        "id": 3,
+        "name": "Zotac RTX 3090",
+        "type": "gpu",
+        "price": 2500,
+        "description": "Lorem ipsum...",
+        "stock": 100,
+        "img": "assets/img/zotac_rtx_3090_1.jpg",
+        "qty": 0
     },{
-        id: 4,
-        name: "B450 Aorus M",
-        type: "mother",
-        price: 100,
-        description: "Lorem ipsum...",
-        stock: 100,
-        img: "assets/img/b450-aorus-m.jpg",
-        qty: 0
+        "id": 4,
+        "name": "B450 Aorus M",
+        "type": "mother",
+        "price": 100,
+        "description": "Lorem ipsum...",
+        "stock": 100,
+        "img": "assets/img/b450-aorus-m.jpg",
+        "qty": 0
     },{
-        id: 5,
-        name: "MSI Z490-A Pro",
-        type: "mother",
-        price: 2500,
-        description: "Lorem ipsum...",
-        stock: 100,
-        img: "assets/img/msi_z490.jpg",
-        qty: 0
+        "id": 5,
+        "name": "MSI Z490-A Pro",
+        "type": "mother",
+        "price": 2500,
+        "description": "Lorem ipsum...",
+        "stock": 100,
+        "img": "assets/img/msi_z490.jpg",
+        "qty": 0
     },{
-        id: 6,
-        name: "Zotac RTX 3090",
-        type: "gpu",
-        price: 2500,
-        description: "Lorem ipsum...",
-        stock: 100,
-        img: "assets/img/zotac_rtx_3090_1.jpg",
-        qty: 0
-    }
-]
+        "id": 6,
+        "name": "Zotac RTX 3090",
+        "type": "gpu",
+        "price": 2500,
+        "description": "Lorem ipsum...",
+        "stock": 100,
+        "img": "assets/img/zotac_rtx_3090_1.jpg",
+        "qty": 0
+    }]
--- js/script.js
+++ js/script.js
@@ -15,25 +15,19 @@ class Product{
         this.img = img;
         this.qty = qty;
     }
-    addIVA(){
-        this.price = this.price * 1.21;
-    }
     sold(){
         this.stock -= 1;
     }
-    addOne(){
-        this.qty += 1;
-    }
 }
 /*Botón ver más */
-let buttonSM = document.getElementById("btn-SM");
-buttonSM.onclick = addCards;
+$("#btn-SM").click(addCards);
 /*Funciones */
 
 function gettingLocalStorage(){
     let cartArray = localStorage.getItem("cart");
     if(cartArray != null){
         let aux = JSON.parse(cartArray);
+        console.log(typeof(aux))
         for (const i of aux){
             let product = new Product(i.id,i.name,i.type,i.price,i.description,i.stock,i.img,i.qty)
             cart.push(product);
@@ -42,24 +36,24 @@ function gettingLocalStorage(){
     }
 }
 function createCartModal(product){
-    let container =  document.createElement("tr");
-    container.id = `cartModalProduct${product.id}`
-    let elementplace =  document.getElementById("cartModalBody");
-    container.innerHTML= `  <td class="w-25">
-                                <img src="${product.img}" class="img-fluid img-thumbnail" alt="Sheep">
-                            </td>
-                            <td>${product.name}</td>
-                            <td>$${product.price}</td>
-                            <td class="qty">${product.qty}</td>
-                            <td>$${product.price * product.qty}</td>
-                            <td>
-                            <a href="#" class="btn btn-danger btn-sm">
-                                <i class="fa fa-times"></i>
-                            </a>
-                            </td>`
-    elementplace.appendChild(container);
+    newElement(`#cartModalBody`,`<tr id="cartModalProduct${product.id}">
+                                                    <td class="w-25">
+                                                        <img src="${product.img}" class="img-fluid img-thumbnail" alt="Sheep">
+                                                    </td>
+                                                    <td>${product.name}</td>
+                                                    <td>$${product.price}</td>
+                                                    <td class="qty">${product.qty}</td>
+                                                    <td>$${product.price * product.qty}</td>
+                                                    <td>
+                                                    <a href="#" class="btn btn-danger btn-sm btn-delete">
+                                                        <i class="fa fa-times"></i>
+                                                    </a>
+                                                    </td>
+                                                </tr>`)
+  $(".btn-delete").click(() => deleteCartItem(product));
+
 }
-function refreshCardModal(product){
+function refreshCartModal(product){
     let element = document.getElementById(`cartModalProduct${product.id}`);
     element.innerHTML= `  <td class="w-25">
                                 <img src="${product.img}" class="img-fluid img-thumbnail" alt="Sheep">
@@ -69,10 +63,11 @@ function refreshCardModal(product){
                             <td class="qty">${product.qty}</td>
                             <td>$${product.price * product.qty}</td>
                             <td>
-                            <a href="#" class="btn btn-danger btn-sm">
+                            <a href="#" class="btn btn-danger btn-sm btn-delete">
                                 <i class="fa fa-times"></i>
                             </a>
                             </td>`
+    $(".btn-delete").click(() => deleteCartItem(product));
 }
 function addCards(){
     for (counter; counter <auxCounter; counter++){
@@ -84,80 +79,78 @@ function addCards(){
 }
 
 function newProduct(product){
-    let container = document.createElement("div");
-    let elementplace = document.getElementById("card-deck");
-    container.className = "card col-md-4";
-    container.innerHTML = `
-                            <div class="card-head">
-                                <img src="${product.img}" alt="" class="card-img-top w-100">
-                            </div>
-                                <p class="card-product">${product.name}</p>
-                                <span class="card-price">$${product.price}</span>
-                                <div class="card-button">
-                                <button type="button" class="btn btn-light" data-toggle="modal" data-target="#product${product.id}"><i class="gg-eye"></i></button>
-                                <button id="${product.id}" class="btn btn-primary">Agregar al carrito</button>
+    newElement("#card-deck",`<div class="card col-md-4">
+                                <div class="card-head">
+                                    <img src="${product.img}" alt="" class="card-img-top w-100">
                                 </div>
-                            `
-    elementplace.appendChild(container);
-    newModal(product);
-    let addBtn = document.getElementById(product.id);
-    addBtn.onclick = () =>{
-            if(cart == ""){
-                cart.push(product);
+                                    <p class="card-product">${product.name}</p>
+                                    <span class="card-price">$${product.price}</span>
+                                    <div class="card-button">
+                                    <button type="button" class="btn btn-light" data-toggle="modal" data-target="#product${product.id}"><i class="gg-eye"></i></button>
+                                    <button id="${product.id}" class="btn btn-primary">Agregar al carrito</button>
+                                    </div>
+                            </div>`)
+    newElement("#card-deck",`<div class="modal fade" id="product${product.id}" tabIndex="-1" role="dialog" aria-labelledby="modalProduct${product.id}" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="modalProduct${product.id}">${product.name}</h5>
+                                            </button>
+                                            </div>
+                                        <div class="modal-body">
+                                            <img class="modal-img" src="${product.img}"></img>
+                                            <div class="modal-description">
+                                                <p>Nombre: ${product.name}</p>
+                                                <p>Precio: $${product.price}</p>
+                                                <p>Descripción: ${product.description}</p>
+
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`);
+    $(`#${product.id}`).click( function () {
+            if (cart == "") {
                 product.qty = 1;
-                refreshCardModal(product);
-            }else{
-                for(let i = 0; i <= cart.length; i++){
-                if(cart[i] == undefined){
-                    cart.push(product);
-                    product.qty = 1;
-                    refreshCardModal(cart[i]);
-                    break;
-                }else if (product.id == cart[i].id){
-                    cart[i].qty += 1;
-                    refreshCardModal(cart[i]);
-                    break;
-                }}
-        }
+                cart.push(product);
+                createCartModal(product);
+            } else {
+                for (let i = 0; i <= cart.length; i++) {
+                    if (cart[i] == undefined) {
+                        product.qty = 1;
+                        cart.push(product);
+                        createCartModal(product);
+                        break;
+                    } else if (product.id == cart[i].id) {
+                        cart[i].qty += 1;
+                        product.qty = cart[i].qty;
+                        refreshCartModal(product);
+                        break;
+                    }
+                };
+            }
+            localStorage.setItem("cart",JSON.stringify(cart));
+        });
         
-        localStorage.setItem("cart",JSON.stringify(cart));
+        
+    }
+
+function deleteCartItem(product){
+    console.log(".")
+    for(let i= 0;i<cart.length;i++){
+        if(cart[i].id == product.id){
+            console.log(product.id)
+            cart.splice(i,1)
+        }
     }
 }
-
-function newModal(product){
-    let container = document.createElement("div");
-    let elementPlace = document.getElementById("card-deck");
-    container.className = "modal fade"
-    container.id = `product${product.id}`
-    container.tabIndex = `-1`
-    container.innerHTML= `
-                            <div class="modal-dialog modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="modalProduct${product.id}">${product.name}</h5>
-                                        </button>
-                                        </div>
-                                    <div class="modal-body">
-                                        <img class="modal-img" src="${product.img}"></img>
-                                        <div class="modal-description">
-                                            <p>Nombre: ${product.name}</p>
-                                            <p>Precio: $${product.price}</p>
-                                            <p>Descripción: ${product.description}</p>
-
-                                        </div>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    </div>
-                                </div>
-                            </div>
-                            `
-    elementPlace.appendChild(container);
-    document.getElementById(`product${product.id}`).setAttribute("role", `dialog`);
-    document.getElementById(`product${product.id}`).setAttribute("aria-labelledby", `modalProduct${product.id}`);
-    document.getElementById(`product${product.id}`).setAttribute("aria-hidden", `true`);
+function newElement(key,value){
+    $(key).append(value);
 }
-
 /*Llamada a funciones*/
 gettingLocalStorage();
 addCards();
+
